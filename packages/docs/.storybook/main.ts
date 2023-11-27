9ae7f2082c232c2f653b7c19fb2e11a609167c40
yagovelaa <yagovela@gmail.com>
2023-11-27 11:03:08 -0300

    Fix: Add subpath to storybook on prodution

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -27,5 +27,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
+  }
 };
 export default config;
